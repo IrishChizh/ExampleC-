7f5c332542fc95164c8388657d8c8cb59bd56249
IrishChizh <irishchizh@gmail.com>
2022-08-12 09:41:22 +0300

    Add IntroMethod

---
+++ Example008_IntroMethod/Program.cs
@@ -0,0 +1,38 @@
+﻿int Max( int arg1, int arg2, int arg3);
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+
+ int a1 = 12 ;
+ int b1 = 36 ;
+ int c1 = 47;
+ int a2 = 22;
+ int b2 = 78;
+ int c2 = 100;
+ int a3 = 2;
+ int b3 = 9;
+ int c3 = 1;
+
+int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+int max = Max(max1, max2, max3);
+Console.WriteLine(max);
+
+
+//первый способ нахождения большего числа ( лапша)
+//int max = a1;
+//if (b1 > max) max = b1;
+//if (c1 > max) max = c1;
+
+//if (a2 > max) max = a2;
+//if (b2 > max) max = b2;
+//if (c2 > max) max = c2;
+
+//if (a3 > max) max = a3;
+//if (b3 > max) max = b3;
+//if (c3 > max) max = c3;
